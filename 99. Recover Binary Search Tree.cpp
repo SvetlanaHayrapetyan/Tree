05b47b06bc770ab9dfa41db944341f30b418883b
SvetlanaHayrapetyan <153418228+SvetlanaHayrapetyan@users.noreply.github.com>
2024-11-24 14:43:24 +0400

    99. Recover Binary Search Tree.cpp

--- 99. Recover Binary Search Tree.cpp
+++ 99. Recover Binary Search Tree.cpp
@@ -1,3 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
  public:
   void recoverTree(TreeNode* root) {
@@ -7,8 +18,8 @@ class Solution {
 
  private:
   TreeNode* pred = nullptr;
-  TreeNode* x = nullptr;  // the first wrong node
-  TreeNode* y = nullptr;  // the second wrong node
+  TreeNode* x = nullptr;  
+  TreeNode* y = nullptr;  
 
   void inorder(TreeNode* root) {
     if (root == nullptr)
