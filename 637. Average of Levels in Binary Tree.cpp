0ece8eac4e2e01bd19b3d80d1545d6695dd9d7be
SvetlanaHayrapetyan <153418228+SvetlanaHayrapetyan@users.noreply.github.com>
2024-11-24 14:34:21 +0400

    637. Average of Levels in Binary Tree.cpp

--- 637. Average of Levels in Binary Tree.cpp
+++ 637. Average of Levels in Binary Tree.cpp
@@ -1,48 +1,42 @@
-#include <vector>
-#include <queue>
-
-// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-        queue<TreeNode*> nodesQueue; // Queue to hold the nodes at the current level.
-        nodesQueue.push(root); // Start with the root node.
-        vector<double> averages; // Vector to store the average values of each level.
+        queue<TreeNode*> nodesQueue; 
+        nodesQueue.push(root); 
+        vector<double> averages; 
 
         while (!nodesQueue.empty()) {
-            int levelSize = nodesQueue.size(); // Number of nodes at the current level.
-            long long levelSum = 0; // Sum of the values of nodes at the current level. Use long long to avoid integer overflow.
-
-            // Iterate over all nodes at the current level.
+            int levelSize = nodesQueue.size(); 
+            long long levelSum = 0; 
             for (int i = 0; i < levelSize; ++i) {
-                TreeNode* currentNode = nodesQueue.front(); // Retrieve the front node in the queue.
-                nodesQueue.pop(); // Remove the node from the queue.
-                levelSum += currentNode->val; // Add the node value to the level sum.
-
-                // If the left child exists, add it to the queue for the next level.
+                TreeNode* currentNode = nodesQueue.front(); 
+                nodesQueue.pop();
+                levelSum += currentNode->val; 
+             
                 if (currentNode->left) {
                     nodesQueue.push(currentNode->left);
                 }
-                // If the right child exists, add it to the queue for the next level.
+    
                 if (currentNode->right) {
                     nodesQueue.push(currentNode->right);
                 }
             }
 
-            // Calculate the average for the current level and add it to the result vector.
-            // Casting levelSum to double to get floating point division.
             averages.push_back(static_cast<double>(levelSum) / levelSize);
         }
 
-        return averages; // Return the vector containing averages of each level.
+        return averages; 
     }
 };
